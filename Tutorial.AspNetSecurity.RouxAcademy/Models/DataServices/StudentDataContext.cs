2270976b40db9ed29f69a2fe534eda5877107847
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-17 15:23:44 -0600

    first commit

---
+++ Tutorial.AspNetSecurity.RouxAcademy/Models/DataServices/StudentDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tutorial.AspNetSecurity.RouxAcademy.Models.Student;
+
+namespace Tutorial.AspNetSecurity.RouxAcademy.DataServices
+{
+    public class StudentDataContext: DbContext
+    {
+        public DbSet<CourseGrade> Grades { get; set; }
+       
+        public StudentDataContext(DbContextOptions<StudentDataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}

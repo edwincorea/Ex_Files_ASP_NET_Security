256ebcdf722b5bef8b936aabac42d50389369630
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-17 16:35:30 -0600

    Implement login and logout with Identity.

--- Tutorial.AspNetSecurity.RouxAcademy/Controllers/AccountController.cs
+++ Tutorial.AspNetSecurity.RouxAcademy/Controllers/AccountController.cs
@@ -8,11 +8,17 @@ namespace Tutorial.AspNetSecurity.RouxAcademy.Controllers
 {
     public class AccountController : Controller
     {
+        // User managing API for persisting users.
         private readonly UserManager<IdentityUser> _userManager;
-       
-        public AccountController(UserManager<IdentityUser> userManager)
+        // Authentication API for user sign in/out.
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(
+            UserManager<IdentityUser> userManager, 
+            SignInManager<IdentityUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         
         //
@@ -61,19 +67,50 @@ namespace Tutorial.AspNetSecurity.RouxAcademy.Controllers
         {
             return View();
         }
+
         //
         // POST: /Account/Login
         [HttpPost]
-        public IActionResult Login(LoginViewModel model, string returnUrl = null)
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl = null)
         {
             ViewData["ReturnUrl"] = returnUrl;
             if (ModelState.IsValid)
             {
-              
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Email, 
+                    model.Password, 
+                    model.RememberMe, 
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction(nameof(StudentController.Index), "Student");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                }
             }
 
             // If we got this far, something failed, redisplay form
             return View(model);
+        }
+
+        //
+        // POST: /Account/Logout
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction(nameof(HomeController.Index), "Home");
         }
 
     }

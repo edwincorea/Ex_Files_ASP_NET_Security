8da95416e735b17e694287ec085e4e554b4cab79
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-30 17:26:26 -0600

    Configure IdentityServer resources.

---
+++ Tutorial.AspNetSecurity.TokenService/Config.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Tutorial.AspNetSecurity.TokenService
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                // Custom resource
+                new IdentityResource
+                {
+                    Name = "Role",
+                    UserClaims = new List<string> { "Role" }
+                }
+            };
+        }
+    }
+}

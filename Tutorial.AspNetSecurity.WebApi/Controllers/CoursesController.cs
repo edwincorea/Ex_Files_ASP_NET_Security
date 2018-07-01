eb05da0b8c0d525696daf9ae103fdce768ba1196
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-07-01 12:01:08 -0600

    Define cross-origin resource sharing (CORS) policies.

---
+++ Tutorial.AspNetSecurity.WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorial.AspNetSecurity.WebApi.Controllers
+{
+    [EnableCors("RouxAcademy")]
+    [Route("api/[controller]")]
+    public class CoursesController : Controller
+    {
+        // GET api/courses
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Computer Science", "Biology", "Anthropology" };
+        }
+    }
+}

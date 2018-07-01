7ffb11b9c000b7766c45259e94d52523b5c52404
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-06-30 19:59:22 -0600

    Configure IdentityServer to use Entity Framework.

--- Tutorial.AspNetSecurity.TokenService/Startup.cs
+++ Tutorial.AspNetSecurity.TokenService/Startup.cs
@@ -2,21 +2,45 @@
 using IdentityServer4.Quickstart.UI;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace Tutorial.AspNetSecurity.TokenService
 {
     public class Startup
     {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var migrationAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             services.AddIdentityServer()
-                .AddTemporarySigningCredential() //creates temporary keys for signing tokens
-                .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                .AddInMemoryClients(Config.GetClients())
+                .AddTemporarySigningCredential() // creates temporary keys for signing tokens
+                .AddConfigurationStore(builder => 
+                    builder.UseSqlServer(
+                        Configuration.GetConnectionString("TokenService"), 
+                        options => options.MigrationsAssembly(migrationAssembly))) // persistence storage for resources and clients data
+                .AddOperationalStore(builder => 
+                    builder.UseSqlServer(
+                        Configuration.GetConnectionString("TokenService"),
+                        options => options.MigrationsAssembly(migrationAssembly))) // persistence grant store
                 .AddTestUsers(TestUsers.Users);
 
             services.AddMvc();
